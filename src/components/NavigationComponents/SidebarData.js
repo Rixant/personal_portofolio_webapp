c93e186113a43dbbdfaa11c473a2d0c6e08f4018
rixant <rishantrokaha@gmail.com>
2021-02-11 23:09:47 -0500

    warning removed 2

--- src/components/NavigationComponents/SidebarData.js
+++ src/components/NavigationComponents/SidebarData.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as BsIcons from 'react-icons/bs';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
